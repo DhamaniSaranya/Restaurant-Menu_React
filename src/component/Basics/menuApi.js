7da43e7d08575c41b68b2aae162d1bf145744da3
DhamaniSaranya <dhamanisaranya69@gmail.com>
2022-07-18 12:42:49 +0530

    first commit

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -2,48 +2,48 @@ const MenuApi = [
     {
       id: 1,
       image: "images/maggi.jpg",
-      name: "maggi",
+      name: "Maggi",
       category: "breakfast",
       price: "12₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
     },
   
     {
       id: 2,
       image: "images/allupakoida.jpg",
-      name: "allu pakoida",
+      name: "Aloo Pakora",
       category: "evening",
       price: "20₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Aloo Pakora (Potato Bajji) is a very popular Indian snack where thinly sliced potatoes are coated with a spiced chickpea flour batter and deep-fried until crispy. This 20 minutes snack is best enjoyed with green chutney and masala chai on the side. Try it!",
     },
     {
       id: 3,
       image: "images/corn.jpg",
-      name: "corn",
+      name: "Corn",
       category: "breakfast",
       price: "10₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "The corn came out perfectly cooked and snappy. It was tender, bright yellow, and moist. It had a little bit of browning in the spots where it came in contact with the grill, but overall it remained bright yellow. The flavor, however, was a bit bland.",
     },
     {
       id: 4,
       image: "../images/chola.jpg",
-      name: "chola",
+      name: "Chola",
       category: "lunch",
       price: "50₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Chole is a Punjabi word for chickpeas and masala is a word for spices. So this dish is nothing but chickpeas cooked to perfection with various spices, onions, tomatoes and herbs. Chickpea is a legume that is relished all over India.",
     },
     {
       id: 5,
       image: "../images/pizza.jpg",
-      name: "pizza",
+      name: "Pizza",
       category: "evening",
       price: "80₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot",
     },
     {
       id: 6,
@@ -52,7 +52,7 @@ const MenuApi = [
       category: "dinner",
       price: "180₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "A thali is a combination of various dishes put together to make a whole meal. An average non-vegetarian thali usually contains salad, rice, chappati and gravy or dry chicken/mutton/fish dish to go with it.",
     },
     {
       id: 7,
@@ -61,7 +61,7 @@ const MenuApi = [
       category: "dinner",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "A good cake, as our experts put it, must look beautiful and taste divine. It must have a perfect balance of appearance, texture, and flavour. It must have nothing but the best and freshest ingredients, meticulously mixed in the right proportions and baked to flawless perfection.",
     },
     {
       id: 8,
@@ -70,16 +70,16 @@ const MenuApi = [
       category: "lunch",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "These are smaller in size than most Rajma grown in plains and have a slightly sweetish taste. The combination of Rajma and rice generally lists as a top favorite of North Indians and Nepalis. Rajma is prepared with onion, garlic and many spices in India, and it is one of the staple foods in Nepal.",
     },
     {
       id: 9,
       image: "../images/samosa.jpg",
-      name: "samaso",
+      name: "Samosa",
       category: "evening",
       price: "10₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "A samosa is a fried or baked pastry with a savory filling, including ingredients such as spiced potatoes, onions, and peas. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
     },
   ];
   
